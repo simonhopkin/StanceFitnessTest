5ed874f220665f86e0bf89f5d7ad6d58106f163f
Simon Hopkin <shopkin@nemaura.co.uk>
2024-08-24 10:07:13 +0100

    New Xcode project, using Storyboard (UIKit), sorry

---
+++ StanceFitnessTest/StanceFitnessTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StanceFitnessTest
+//
+//  Created by Simon Hopkin on 24/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
